998b77e5fcb71659c037699f93c4bd8c307e7f2e
MisterGamingTV <68148357+MisterGamingTV@users.noreply.github.com>
2022-02-17 12:48:24 +0100

    Update index.js
    fixed stupid mistake lol

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 
 client.on('messageReactionAdd', (reaction, user) => {
     if(user.bot) return;
-    if(reaction.emoji.name == "⛔"  && reaction.count == 2){
+    if(reaction.emoji.name == "⛔"  && reaction.count == 5){
         console.log(`Reaction!`);
        reaction.message.delete()
        reaction.message.channel.send(`${reaction.message.author}'s message has been deleted lol`)

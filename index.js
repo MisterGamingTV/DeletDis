132048291875113114b4f8a008b48fed55cd2abc
MisterGamingTV <68148357+MisterGamingTV@users.noreply.github.com>
2022-02-17 12:37:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// Require the necessary discord.js classes
+const { Client, Intents, Message } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES] });
+
+// When the client is ready, run this code (only once)
+client.on('ready', () => {
+	console.log(`[LOG] Logged in as ${client.user.tag}!`);
+    client.channels.cache.find(channel => channel.id === 'SOME_CHANNEL_ID').send(`I'm ready!`);
+});
+
+client.on('messageReactionAdd', (reaction, user) => {
+    if(user.bot) return;
+    if(reaction.emoji.name == "⛔"  && reaction.count == 2){
+        console.log(`Reaction!`);
+       reaction.message.delete()
+       reaction.message.channel.send(`${reaction.message.author}'s message has been deleted lol`)
+}});
+
+// Login to Discord with your client's token
+client.login(`TOKEN`);

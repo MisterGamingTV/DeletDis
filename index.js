47d2c9fb5e46e754051c01731fa51a2efc450722
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-02-17 12:57:25 +0100

    @MisterGamingTV schläääääächt

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const { Client, Intents, Message } = require('discord.js');
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// When the client is ready, run this code (only once)
+// When the client is ready, run this code
 client.on('ready', () => {
 	console.log(`[LOG] Logged in as ${client.user.tag}!`);
-    client.channels.cache.find(channel => channel.id === 'SOME_CHANNEL_ID').send(`I'm ready!`);
+	client.channels.cache.find(channel => channel.id === 'SOME_CHANNEL_ID').send(`I'm ready!`);
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
-    if(user.bot) return;
-    if(reaction.emoji.name == "⛔"  && reaction.count == 5){
-        console.log(`Reaction!`);
-       reaction.message.delete()
-       reaction.message.channel.send(`${reaction.message.author}'s message has been deleted lol`)
-}});
+    	if (user.bot) return;
+    	if (reaction.emoji.name == "⛔"  && reaction.count == 5) {
+		console.log("Five reactions caused a message delete");
+		reaction.message.delete();
+		reaction.message.channel.send(`${reaction.message.author}'s message has been deleted!`);
+    	}
+});
 
 // Login to Discord with your client's token
-client.login(`TOKEN`);
+client.login("TOKEN");
